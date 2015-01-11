755ab6be7b2877a53d59db123c5fd339b95b662e
Nat Pryce <sw@natpryce.com>
2015-01-11 00:00:42 +0000

    Fix javadoc on Java 8

--- src/main/java/org/hamcrest/junit/MatcherAssume.java
+++ src/main/java/org/hamcrest/junit/MatcherAssume.java
@@ -20,7 +20,7 @@ import org.junit.AssumptionViolatedException;
  * }
  * </pre>
  * These methods can be used directly: <code>Assume.assumeTrue(...)</code>, however, they
- * read better if they are referenced through static import:<br/>
+ * read better if they are referenced through static import:
  * <pre>
  * import static org.junit.Assume.*;
  *    ...
